57c4de2e4f7f111e3d67aa47375531d07da0792f
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-02-27 12:32:07 +0000

    Update for Heroku deploy

--- env.sh
+++ env.sh
@@ -10,3 +10,8 @@ cp -prf /home/workspace/.ssh ~/
 # git setup
 git config --global user.email "34284486+yamatokataoka@users.noreply.github.com"
 git config --global user.name "yamatokataoka"
+
+# Heroku
+add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
+curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
+apt-get update && apt-get install heroku
