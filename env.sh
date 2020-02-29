e8e86da8074b888c44b1d3fae90e7bdf00eb0133
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-02-29 01:31:25 +0000

    Refactor heroku installation

--- env.sh
+++ env.sh
@@ -11,7 +11,5 @@ cp -prf /home/workspace/.ssh ~/
 git config --global user.email "34284486+yamatokataoka@users.noreply.github.com"
 git config --global user.name "yamatokataoka"
 
-# Heroku
-add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
-curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
-apt-get update && apt-get install heroku
+# install Heroku
+curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
